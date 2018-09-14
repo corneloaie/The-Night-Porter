2a121bb0cf32718b4247fec01e49bb1bb733590e
Cornel Oaie <corneloaie@gmail.com>
2018-09-14 18:22:46 +0300

    chapter 3 demo accessing subviews

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func enableDarkMode(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+        let everything  = view.subviews
+        for eachView in everything {
+            if eachView is UILabel{
+                let currentLabel = eachView as? UILabel
+                currentLabel?.textColor = UIColor.lightGray
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
